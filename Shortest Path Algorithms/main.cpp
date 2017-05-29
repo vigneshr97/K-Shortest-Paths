b70fa94b1e1506c471359d98fcde1690db9683d9
RADHAKRISHNAN <ce14b036@smail.iitm.ac.in>
2017-05-29 11:32:11 +0530

    Create main.cpp

---
+++ Shortest Path Algorithms/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "basic.h"
+#include "network.h"
+#include "spath.h"
+using namespace std;
+int main()
+{
+	network net;
+	net.inputdata();
+	net.outputdata();
+	return 0;
+}
